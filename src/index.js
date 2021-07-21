9c0738fa4fdb23691b796827a83b771834f29754
DESKTOP-N73DN8E\a3955 <a39550897@gmail.com>
2021-07-21 19:16:51 +0900

    highlight

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { CopyBlock, dracula } from "react-code-blocks";
+import { sample, TopBar } from "./components";
+import Logo from "./components/Logo";
+import "./styles.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [language, changeLanguage] = useState("jsx");
+  const [languageDemo, changeDemo] = useState(sample["jsx"]);
+  const [lineNumbers, toggleLineNumbers] = useState(true);
+  return (
+    <div className="container mx-auto p-4">
+      <Logo height="150px" width="100%" />
+      <TopBar
+        language={{
+          value: language,
+          onChange: e => {
+            changeDemo(sample[e.target.value]);
+            return changeLanguage(e.target.value);
+          },
+          options: Object.keys(sample).map(lang => (
+            <option key={lang} value={lang}>
+              {lang}
+            </option>
+          ))
+        }}
+        toggle={{
+          checked: lineNumbers,
+          onChange: e => toggleLineNumbers(!lineNumbers)
+        }}
+      />
+      <div className="demo">
+        <CopyBlock
+          language={language}
+          text={languageDemo}
+          showLineNumbers={lineNumbers}
+          theme={dracula}
+          wrapLines={true}
+          codeBlock
+        />
+      </div>
+    </div>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
